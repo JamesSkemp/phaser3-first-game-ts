a34e248919da28a11c869504d51a442e88e56d92
James Skemp <strivinglife@gmail.com>
2022-10-15 11:20:22 -0500

    Add arcade physics and platforms

--- src/ts/Scenes/MainGame.ts
+++ src/ts/Scenes/MainGame.ts
@@ -5,6 +5,7 @@ export default class MainGame extends Phaser.Scene {
 	 * Unique name of the scene.
 	 */
 	public static Name = "MainGame";
+	platforms: Phaser.Physics.Arcade.StaticGroup;
 
 	public preload(): void {
 		// Preload as needed.
@@ -15,5 +16,13 @@ export default class MainGame extends Phaser.Scene {
 
 		//this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, "Phaser-Logo-Small");
 		this.add.image(400, 300, 'sky');
+
+		this.platforms = this.physics.add.staticGroup();
+
+		this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+		this.platforms.create(600, 400, 'ground');
+		this.platforms.create(50, 250, 'ground');
+		this.platforms.create(750, 220, 'ground');
 	}
 }
